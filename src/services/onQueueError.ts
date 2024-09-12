a0079a1f618cc90956b575ce6db5e0e92a137d6d
MD Taukir Alam <md.taukir.alam@g.bracu.ac.bd>
2024-09-12 19:03:22 +0600

    fixed an error case : if the ai model is down

--- src/services/onQueueError.ts
+++ src/services/onQueueError.ts
@@ -1,28 +1,20 @@
 import { ErrorTypes } from "../enums";
 import { TtdResponseFail } from "../models/ttdProcessorResponseFail";
-import {
-  TtdQueueErrorModel,
-  TtdQueueInvalidModel,
-  TtdQueueTimeOutModel,
-} from "../models/ttdQueueErrorTypesModel";
+import { TtdQueueErrorModel, TtdQueueInvalidModel, TtdQueueTimeOutModel } from "../models/ttdQueueErrorTypesModel";
 import { TtdQueue } from "../models/ttdQueueModel";
 import { deleteFromTtdQueue } from "../utils/deleteFromQueue";
 
-export async function onQueueError(
-  oldestDocuemnt: TtdQueue,
-  apiResponse: TtdResponseFail
-) {
+export async function onQueueError(oldestDocuemnt: TtdQueue, apiResponse: TtdResponseFail) {
   const { _id, q_status, ...rest } = oldestDocuemnt.toObject();
 
   const documentData = {
-    rest,
+    ...rest,
     ttd_processor_api_response: apiResponse,
   };
 
   const { error_type } = apiResponse;
-  console.log(`| video processing error, type:${error_type}`);
-  console.log(`defined as :${ErrorTypes.INVALID}`);
-
+  console.log(`| Text processing error, type:${error_type}`);
+  console.log(`| Text processing error data : ${documentData}`);
   try {
     switch (error_type) {
       case ErrorTypes.TIMEOUT:
